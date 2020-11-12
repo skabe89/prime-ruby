1a6b4d7043085bd51dea9abdb76c8fab700fb083
Sean <skabe89@gmail.com>
2020-11-11 20:25:06 -0500

    commit tree d2df2d4fd7d9fd4c4125d9e2351261b1feda3c7b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(num)
+    div = [2, 3, 5, 7, 41]
+    if num <= 1
+        false
+    elsif div.any?(num)
+        true
+    elsif div.each do |i|
+        return false if num % i == 0
+    end
+        true
+    end
+end
